773460b518ef7fc4a5248f71692e7d2484fc35d2
DannyHucc <dannyhu10011101@gmail.com>
2023-03-12 10:29:04 +0800

    A3

---
+++ AC-S2-2-A3/main.js
@@ -0,0 +1,26 @@
+// 使用嚴格模式
+'use strict'
+
+// DEFAULT CODE ////////////////////////
+const BASE_URL = 'https://webdev.alphacamp.io/api/lyrics/'
+const songList = document.querySelector('#song-list')
+const lyricsPanel = document.querySelector('#lyrics-panel')
+const album = {
+    artist: 'Adele',
+    album: '25',
+    tracks: [
+        'Hello',
+        'Send My Love (To Your New Lover)',
+        'I Miss You',
+        'When We Were Young',
+        'Remedy',
+        'Water Under the Bridge',
+        'River Lea',
+        'Love in the Dark',
+        'Million Years Ago',
+        'All I Ask',
+        'Sweetest Devotion'
+    ]
+}
+
+// WRITE YOUR CODE ////////////////////////
