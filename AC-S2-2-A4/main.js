0fd028e6fd2239708307201d850dc6b7dd44e0b3
DannyHucc <dannyhu10011101@gmail.com>
2023-03-13 21:47:25 +0800

    A4

--- AC-S2-2-A4/main.js
+++ AC-S2-2-A4/main.js
@@ -16,13 +16,20 @@ const todos = [
     "Pay bills"
 ]
 
+function test(text, className = '') {
+
+    className = className.slice(1, className.length - 1)
+    console.log(typeof text, typeof className)
+    console.log(text, className)
+}
+
 // 新增內容
-function addItem(text, labelClass='') {
+function addItem(text, className = '') {
     let newItem = ""
-    newItem = 
-    `
+    newItem =
+        `
     <li>
-        <label for="todo" class="${labelClass}">${text}</label>
+        <label for="todo" class="${className}">${text}</label>
         <i class="delete fa fa-trash"></i>
     </li>
     `
@@ -30,37 +37,40 @@ function addItem(text, labelClass='') {
 }
 
 // 創建輸入功能
-function inputValues (event) {
+function inputValues(event) {
     const inputValue = input.value
     if ((inputValue.length > 0) && // 判斷有輸入內容
         (inputValue.trim() !== "") // 判斷不是空白
-        )
-    {
+    ) {
         // 判斷事件為 click
         if (event.type.toString() === 'click') {
-            todoList.innerHTML += addItem(inputValue) // 新增內容
-        // 判斷事件為 keyup
+            const className = 'todo'
+            todoList.innerHTML += addItem(inputValue, className) // 新增內容
+            input.value = ""
+            // 判斷事件為 keyup
         } else if (event.key.toString() === 'Enter') {
             todoList.innerHTML += addItem(inputValue) // 新增內容
+            input.value = ""
         }
     }
 };
 
 // move and delete
-function moveAndDelete (event) {
+function moveAndDelete(event) {
     const target = event.target
     const parentElement = target.parentElement
     if (target.classList.contains("delete")) {
         deleteList(event) // 刪除元素
     } else if (target.tagName === "LABEL") { // 當點擊 label
         // 增加至 done list 裏面
-        doneList.innerHTML += addItem(parentElement.textContent, 'checked')
+        const className = 'checked'
+        doneList.innerHTML += addItem(parentElement.textContent, className) // 新增內容
         deleteList(event) // 刪除元素
     }
 }
 
 // delete list
-function deleteList (event) {
+function deleteList(event) {
     const target = event.target
     const parentElement = target.parentElement
     parentElement.remove() // 刪除元素
@@ -73,9 +83,12 @@ for (let todo of todos) {
 
 // 建立按鈕的事件
 addBtn.addEventListener("click", inputValues)
+
 // 建立鍵盤的事件
 input.addEventListener("keyup", inputValues)
+
 // 建立點擊 todo list 的事件
 todoList.addEventListener("click", moveAndDelete)
+
 // 建立點擊 done list 的事件
 doneList.addEventListener("click", deleteList)
